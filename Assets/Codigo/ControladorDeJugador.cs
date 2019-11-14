af50f680df6ba83a9225ea11c5fca1943cf2bd84
CabezasJoaquin <jcb273@hotmail.es>
2019-11-14 14:25:49 +0100

    ControladorDeJugador

---
+++ Assets/Codigo/ControladorDeJugador.cs
@@ -0,0 +1,27 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class ControladorDeJugador : MonoBehaviour
+{
+    //Almacena componetne Rigidbody del personaje Jugador
+    Rigidbody rb;
+    public void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void Update()
+    {
+        
+    }
+
+    public void FixedUpdate()
+    {
+        float movimientoHorizontal = Input.GetAxis("Horizontal");
+        float movimientoVertical = Input.GetAxis("Vertical");
+        
+        Vector3 movimiento = new Vector3(movimientoHorizontal, 0f, movimientoVertical);
+        rb.AddForce(movimiento);
+    }
+}
