a619dc905c20e298281201da100a684560ca9b72
CabezasJoaquin <jcb273@hotmail.es>
2019-11-15 11:13:21 +0100

    JCB juego 15/11/2019

--- Assets/Codigo/ControladorDeJugador.cs
+++ Assets/Codigo/ControladorDeJugador.cs
@@ -1,11 +1,18 @@
-﻿
+﻿using System.Collections;
 using UnityEngine;
-using System.Collections;
+using UnityEngine.UI;
 
 public class ControladorDeJugador : MonoBehaviour
 {
     //Almacena componetne Rigidbody del personaje Jugador
     Rigidbody rb;
+
+    public float velocidad;
+    int contador;
+
+    public Text Marcador;
+    public Text FinJuego;
+
     public void Awake()
     {
         rb = GetComponent<Rigidbody>();
@@ -22,6 +29,18 @@ public class ControladorDeJugador : MonoBehaviour
         float movimientoVertical = Input.GetAxis("Vertical");
         
         Vector3 movimiento = new Vector3(movimientoHorizontal, 0f, movimientoVertical);
-        rb.AddForce(movimiento);
+        rb.AddForce(movimiento*velocidad);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+        contador = contador + 1;
+        ActualizaMarcador();
+    }
+
+    void ActualizaMarcador()
+    {
+        Marcador.text = "Puntos: " + contador;
     }
 }
