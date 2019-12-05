d5436de2d151a02e42b2695f764b21649b97faf7
CabezasJoaquin <jcb273@hotmail.es>
2019-12-05 10:00:22 +0100

    05/12/2019

---
+++ Assets/Codigo/MovementScritp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScritp : MonoBehaviour { 
+
+    public GameObject destino;
+private UnityEngine.AI.NavMeshAgent agenteDeNavegacion;
+
+// Use this for initialization
+void Start()
+{
+    agenteDeNavegacion = gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
+    agenteDeNavegacion.SetDestination(destino.transform.position);
+
+}
+}
